facad081cb64e0622d27dc27e4f8d4af9dc068e3
dtmemories <dtmemories1@gmail.com>
2025-02-21 11:49:03 +0530

    config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'session id', //add your session id
+SESSION_ID: process.env.SESSION_ID || 'SUPUN-MD=3dMA1YDa#koUg5Nh3ankZC07EoNwC6BzdVHS-bpz-bf1eei4lF0c', //add your session id
 MONGODB: process.env.MONGODB || "mongodb url", //add your mongodb url
  URL : process.env.BOT_MENU_LINKS || 'https://i.ibb.co/bHXBV08/9242c844b83f7bf9.jpg',
 MAX_SIZE: 500
